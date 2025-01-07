b088a7c28d3d1cb23f3dc63aedc534b6bc835b45
Félix Piédallu <felix@piedallu.me>
2025-01-07 14:21:29 +0100

    Helpers v2.1, and cleanup

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #=================================================
-# RETRIEVE ARGUMENTS FROM THE MANIFEST
+# COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
 # Transfer the main SSO domain to the App:
@@ -25,49 +25,17 @@ log_level="WARNING"
 # e.g.: point pip cache to: /var/www/$app/.cache/
 XDG_CACHE_HOME="$install_dir/.cache/"
 
-log_path=/var/log/$app
-log_file="${log_path}/${app}.log"
-
-#=================================================
-# HELPERS
-#=================================================
-
 myynh_setup_python_venv() {
     # Always recreate everything fresh with current python version
-    ynh_safe_rm "$install_dir/venv"
+    if [ -d "$install_dir/venv" ]; then
+        ynh_safe_rm "$install_dir/venv"
+    fi
 
-    chown -R "$app:$app" "$install_dir"
     # Skip pip because of: https://github.com/YunoHost/issues/issues/1960
     ynh_exec_as_app python3 -m venv --without-pip "$install_dir/venv"
 
-    ynh_exec_as_app $install_dir/venv/bin/python3 -m ensurepip
+    ynh_exec_as_app "$install_dir/venv/bin/python3" -m ensurepip
     # using --no-cache-dir option because user doesn't have permission to write on cache directory (don't know if it's on purpose or not)
-    ynh_exec_as_app $install_dir/venv/bin/pip3 install --no-cache-dir --upgrade wheel pip setuptools
-    ynh_exec_as_app $install_dir/venv/bin/pip3 install --no-cache-dir -r "$install_dir/requirements.txt"
-}
-
-myynh_setup_log_file() {
-    (
-        set -x
-
-        mkdir -p "$(dirname "$log_file")"
-        touch "$log_file"
-
-        chown -c -R $app:$app "$log_path"
-        chmod -c o-rwx "$log_path"
-    )
-}
-
-myynh_fix_file_permissions() {
-    (
-        set -x
-
-        # /home/yunohost.app/$app/
-        chown -c -R "$app:" "$data_dir"
-        chmod -c o-rwx "$data_dir"
-
-        # /var/www/$app/
-        chown -c -R "$app:" "$install_dir"
-        chmod -c o-rwx "$install_dir"
-    )
+    ynh_exec_as_app "$install_dir/venv/bin/pip3" install --no-cache-dir --upgrade wheel pip setuptools
+    ynh_exec_as_app "$install_dir/venv/bin/pip3" install --no-cache-dir -r "$install_dir/$requirements_path"
 }
